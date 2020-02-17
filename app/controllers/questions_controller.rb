0a516fafdca971ebdb66ce4b19e18a55686a674f
thibault froissant <thibault.froissant@gmail.com>
2020-02-17 15:05:53 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:ask]
+      if @question == 'I am going to work'
+          @answer = 'Great!'
+        elsif @question.include?('?')
+          @answer = 'Silly question, get dressed and go to work'
+        else @answer = 'I dont care, get dressed and go to work!'
+      end
+  end
+end
